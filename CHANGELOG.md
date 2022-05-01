213d1167ddaee1159ece09749ea4a3f538fe106f
CraftedCart <CraftedCart@outlook.com>
2022-05-01 16:56:25 +0100

    Added default username/password options

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,3 +1,13 @@
-- Pressing a button (A/B/C/D) for less than a frame will now hold that button for one frame, to prevent dropping
-  quickly pressed inputs
+- On keyboard, pressing a button (A/B/C/D) for less than a frame will now hold that button for one frame, to prevent
+  dropping quickly pressed inputs
 - New config option `patches.skip_legal` (default `true`) lets you skip the legal screen on startup
+- New config options `patches.default_username` and `patches.default_password` let you set, well, the default username
+  and password so you don't have to enter it manually. Note that the default password only works if a username is set,
+  and only for that username.
+
+  ```
+  patches {
+  	default_username BOB
+  	default_password ABCDAB
+  }
+  ```
--- tgm3_input/main.cpp
+++ tgm3_input/main.cpp
@@ -19,6 +19,17 @@ base_input *devices[] = {
 
 const config cfg("tgm3.cfg");
 
+static char default_username[3];
+
+// Each password char takes up 4 bits, A=0x8 B=0x4 C=0x2 D=0x1
+static unsigned char default_password[3];
+
+static const unsigned char PASSWORD_A = 0x08;
+static const unsigned char PASSWORD_B = 0x04;
+static const unsigned char PASSWORD_C = 0x02;
+static const unsigned char PASSWORD_D = 0x01;
+static const unsigned char PASSWORD_INVALID = 0x00;
+
 using window_proc_t = LRESULT(CALLBACK*)(HWND, UINT, WPARAM, LPARAM);
 static window_proc_t orig_window_proc;
 /**
@@ -214,13 +225,106 @@ void hook_position_window(const char fullscreen)
 	ChangeDisplaySettingsEx(info.szDevice, &dev_mode, nullptr, CDS_FULLSCREEN, nullptr);
 }
 
-static void patch_ptr(unsigned int offset, unsigned int value) {
+struct name_entry
+{
+	char name[3];
+	char unk_0x3;
+	char unk_0x4;
+	char unk_0x5;
+	char unk_0x6;
+	char unk_0x7;
+	unsigned int unk_0x8;
+	unsigned short unk_0xc;
+	char num_entered_chars;
+	char selected_char_index;
+	char unk_0x10;
+	char unk_0x11;
+};
+
+using initialize_name_entry_t = void (*)(name_entry*, unsigned int);
+static initialize_name_entry_t orig_initialize_name_entry;
+
+static void hook_initialize_name_entry(name_entry* entry, unsigned int param2)
+{
+	orig_initialize_name_entry(entry, param2);
+
+	// Set our name
+	entry->name[0] = default_username[0];
+	entry->name[1] = default_username[1];
+	entry->name[2] = default_username[2];
+
+	// Say that we've entered 3 chars
+	entry->num_entered_chars = 3;
+
+	// Set the currently entered char to END (not really necessary, but it looks nicer)
+	entry->selected_char_index = 0x29;
+}
+
+__declspec(naked) static void hook_verify_username()
+{
+	_asm {
+		// We clobber the CL and EAX registers, but that's OK since our clobbered values aren't read beyond this hook
+
+		// Jump to `skip` if the 1st username char isn't the 1st default username char
+		mov cl, [ebp + 0x18 + 0]
+		cmp cl, default_username[0]
+		jne skip
+
+		// Jump to `skip` if the 2nd username char isn't the 2nd default username char
+		mov cl, [ebp + 0x18 + 1]
+		cmp cl, default_username[1]
+		jne skip
+
+		// Jump to `skip` if the 3rd username char isn't the 3rd default username char
+		mov cl, [ebp + 0x18 + 2]
+		cmp cl, default_username[2]
+		jne skip
+
+		// Set our password
+		// unknown.password_entry->password[0] = default_password[0];
+		mov cl, default_password[0]
+		mov [ebp + 0x2C + 0], cl
+		// unknown.password_entry->password[1] = default_password[1];
+		mov cl, default_password[1]
+		mov [ebp + 0x2C + 1], cl
+		// unknown.password_entry->password[2] = default_password[2];
+		mov cl, default_password[2]
+		mov [ebp + 0x2C + 2], cl
+
+		// Say that we've entered 6 chars
+		// unknown.password_entry->num_entered_chars = 6;
+		mov [ebp + 0x2C + 6], 6
+
+		skip:
+
+		// Jump back to the end of the `switch` where we came from (since we overwrite a `break` statement/`jmp` instruction)
+		mov eax, 0x0041593E
+		jmp eax
+	}
+}
+
+static void patch_ptr(unsigned int offset, unsigned int value)
+{
 	DWORD old_protect;
 	VirtualProtect((void*)(offset), 4, PAGE_READWRITE, &old_protect);
 
 	*((unsigned int*)offset) = value;
 
 	VirtualProtect((void*)(offset), 4, old_protect, &old_protect);
+}
+
+static unsigned char convert_password_char(char c) {
+	if (c == 'A' || c == 'a') {
+		return PASSWORD_A;
+	} else if (c == 'B' || c == 'b') {
+		return PASSWORD_B;
+	} else if (c == 'C' || c == 'c') {
+		return PASSWORD_C;
+	} else if (c == 'D' || c == 'd') {
+		return PASSWORD_D;
+	} else {
+		return PASSWORD_INVALID;
+	}
 }
 
 /**
@@ -242,14 +346,62 @@ int CALLBACK hook_WinMain(
 	for (auto &device : devices)
 		device->init(cfg);
 
+	bool has_default_username = false;
+	bool has_default_password = false;
+	std::string default_username_str = cfg.value_str("", "patches.default_username");
+	if (default_username_str.size() > 0) {
+		// Ensure the string is padded to at least 3 chars long
+		default_username_str += "  ";
+
+		has_default_username = true;
+		default_username[0] = default_username_str[0];
+		default_username[1] = default_username_str[1];
+		default_username[2] = default_username_str[2];
+
+		std::string default_password_str = cfg.value_str("", "patches.default_password");
+		if (default_password_str.size() == 6) {
+			unsigned char char1 = convert_password_char(default_password_str[0]);
+			unsigned char char2 = convert_password_char(default_password_str[1]);
+			unsigned char char3 = convert_password_char(default_password_str[2]);
+			unsigned char char4 = convert_password_char(default_password_str[3]);
+			unsigned char char5 = convert_password_char(default_password_str[4]);
+			unsigned char char6 = convert_password_char(default_password_str[5]);
+
+			if (
+				char1 != PASSWORD_INVALID &&
+				char2 != PASSWORD_INVALID &&
+				char3 != PASSWORD_INVALID &&
+				char4 != PASSWORD_INVALID &&
+				char5 != PASSWORD_INVALID &&
+				char6 != PASSWORD_INVALID
+			) {
+				has_default_password = true;
+				default_password[0] = char1 | (char2 << 4);
+				default_password[1] = char3 | (char4 << 4);
+				default_password[2] = char5 | (char6 << 4);
+			}
+		}
+	}
+
 	DetourFunction((BYTE*)(0x452CE0), (BYTE*)(hook_set_play_mode));
 	DetourFunction((BYTE*)(0x434E00), (BYTE*)(hook_set_sprite_scale));
+
 	orig_position_window = (position_window_t)(DetourFunction(
 		(BYTE*)(0x450E50), (BYTE*)(hook_position_window)));
 	orig_window_proc = (window_proc_t)(DetourFunction(
 		(BYTE*)(0x451400), (BYTE*)(hook_window_proc)));
 	orig_get_jvs_data = (get_jvs_data_t)(DetourFunction(
 		(BYTE*)(0x45D490), (BYTE*)(hook_get_jvs_data)));
+
+	if (has_default_username) {
+		orig_initialize_name_entry = (initialize_name_entry_t)(DetourFunction(
+			(BYTE*)(0x40E950), (BYTE*)(hook_initialize_name_entry)));
+	}
+	if (has_default_password) {
+		// This hooks a `break` (jmp) statement in a switch, that we then jump back to at the end of the hook function
+		DetourFunction((BYTE*)(0x415604), (BYTE*)(hook_verify_username));
+	}
+
 
 	// Demo playback
 	if (cmdline != nullptr && *cmdline != '\0')
@@ -261,7 +413,8 @@ int CALLBACK hook_WinMain(
 
 	// Skip the legal screen
 	if (cfg.value_bool(true, "patches.skip_legal")) {
-		patch_ptr(0x00406758, 0x00406710);
+		// patch_ptr(0x00406758, 0x00406710); // Make the first screen the ARIKA screen
+		patch_ptr(0x00406758, 0x0040671B); // Make the first screen the title screen
 	}
 
 	// Call the original startup function
